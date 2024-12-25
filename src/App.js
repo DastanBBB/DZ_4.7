0ad885dce9d29edfa555aaed5dd49f1d78661ea3
DastanBBB <dasbuk0812@gmail.com>
2024-12-26 00:08:04 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodosPage from "./pages/todosPage/TodosPage";
+
+function App() {
+  return (
+    <div className="App">
+        <TodosPage/>
+    </div>
+  );
+}
+
+export default App;
